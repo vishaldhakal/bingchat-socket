0960d8a66671e53b73a13665bfcccbc5bbaf71d8
vishaldhakal <vishaldhakal96@gmail.com>
2024-02-28 20:24:52 +0545

    dfV

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ async def main(websocket, path) -> None:
                 await websocket.send(response)
 
 if __name__ == "__main__":
-    start_server = websockets.serve(main, "localhost", 8000)
+    start_server = websockets.serve(main, "localhost", 10000)
 
     asyncio.get_event_loop().run_until_complete(start_server)
     asyncio.get_event_loop().run_forever()
