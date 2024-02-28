eb0dd8e02f18d04d55e2a1e00a31de46ef3c517e
vishaldhakal <vishaldhakal96@gmail.com>
2024-02-28 19:58:15 +0545

    dfV

---
+++ main.py
@@ -0,0 +1,23 @@
+import asyncio
+import websockets
+from sydney import SydneyClient
+
+async def main(websocket, path) -> None:
+    async with SydneyClient() as sydney:
+        while True:
+            prompt = await websocket.recv()
+
+            if prompt == "!reset":
+                await sydney.reset_conversation()
+                continue
+            elif prompt == "!exit":
+                break
+
+            async for response in sydney.ask_stream(prompt):
+                await websocket.send(response)
+
+if __name__ == "__main__":
+    start_server = websockets.serve(main, "localhost", 8765)
+
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_forever()
